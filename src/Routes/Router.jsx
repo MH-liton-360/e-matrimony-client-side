b9ee28200db4be2e536d58f96893818815884b18
MH-liton-360 <liton72524nk@gmail.com>
2025-05-05 22:45:55 +0600

    first commit

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,9 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layouts/Main";
 
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>,
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element:
+            }
+        ]
     },
 ]);
